5f680bd6d0ab74a8becf15b428fddf800844648d
cb <cb@f8efed9a-17d7-42fd-b042-a1a24eb3e7d8>
2008-04-15 20:32:10 +0000

    First working version. Retrieves recursively all file
    git-svn-id: https://svn.256bit.org/svn/ftpbackup@4 f8efed9a-17d7-42fd-b042-a1a24eb3e7d8

--- perl_backup.pl
+++ perl_backup.pl
@@ -18,109 +18,84 @@ sub FTPinit;
 sub FTPlist;
 sub FTPinitLocal;
 sub FTPcheckOldVers;
+sub FTPgetFiles;
 
 
-sub getConfig(){#{{{
-	
-    # Default Options, these can be overruled
-    # using commandline optins
-	my $user		 = "chrisbra";
-	my $password	 = "b97urlington";
-	my $host		 = "localhost";
-	my $localdir	 = "~/perl_backup";
-	# FTP control port
-	my $port		 = 21;
-	# Enable Passive mode? 1 enables, 0 disables
-	my $passive      = 1;
-	# Enable Recurisve mode? 1 enables, 0 disables
-	my $recursive    = 1;
-	# Enable Debug mode? 1 enables, 0 disables
-	my $debug		 = 1;
-	# Enable binary mode? 1 enables, 0 disables
-	my $binary		 = 1;
-	# Wieviele Versionen behalten:
-	my $keep		 = 3;
+my %config = getConfig();
 
-	# exclude patterns
-	my $exclude;
-
-	# Operating System specific
-	my $ospath  = ($Config{"osname"} =~ "MsWin" ) ? '\\' : '/';
-
-	GetOptions('user=s' => \$user,
-               'pass=s' => \$password,
-               'help'  => sub {usage},
-               'server=s' => \$host,
-               'recursive' => \$recursive,
-               'port=i' => \$port,
-		       'passive=i' => \$passive,
-		       'debug=i'   => \$debug,
-			   'binary=i'  => \$binary,
-			   'localdir=s' => \$localdir);
-
-	my %config = (
-		server	 =>  $host,
-		user	 =>  $user,
-		pass	 =>  $password,
-		passive	 =>	 $passive,
-		debug	 =>	 $debug,
-		binary	 =>	 $binary,
-		localdir =>  $localdir,
-		ospath   =>  $ospath,
-		keep	 =>	 $keep
-	);
-	$config{"localdir"}=glob($config{"localdir"});
-
-	return %config;
-}#}}}
-
-sub vprint {#{{{
-	my ($config, $msg) = @_;
-	if ($config->{"debug"}) {
-		print "[debug] $msg\n";
-	}
-}#}}}
-
-my %config;
-%config = getConfig();
-
-vprint(\%config,"Server: $config{'server'}");
+vprint(\%config,"Server: $config{'server'}", "debug");
 my $ftp=FTPinit(\%config);
 
 my @temp = glob($config{'localdir'} . $config{'ospath'} . "20*");
 FTPcheckOldVers(\@temp, \%config);
 
-my $backup_dir = FTPinitLocal(\$ftp, \%config);
-vprint(\%config, "Backing up into Directory: $backup_dir");
+my $backup_dir = FTPinitLocal(\%config);
+vprint(\%config, "Backing up into Directory: $backup_dir", "debug");
+
+@temp = FTPlist($ftp, $config{'dir'});
+#foreach (@temp){
+#	print "$_\n";
+#}
+chdir $backup_dir;
+FTPgetFiles($ftp, \@temp, ".", \%config);
 
 
+sub FTPgetFiles {#{{{
+	(my $ftp, my $list, my $ldir, my $config) = @_;
+	mkdir $ldir unless -d $ldir;
+	chdir $ldir;
+	$ftp->cwd($ldir);
+	my $status;
+#	$list =~ s/(\S*)$/$1/g;
+#	print @$list[0];
+	foreach (@$list){
+		my @files = split / +/, $_, 9;
+		if ($_ =~ /^d/){
+			vprint($config, "Downloading directory $files[8]", "debug");
+			my @temp = $ftp->dir($files[8]);
+			#print "$_\n" foreach (@temp);
+			FTPgetFiles($ftp, \@temp, $files[8], $config);
+		}
+		else {
+			vprint($config, "Downloading file $files[8]", "debug");
+			$status = $ftp->get($files[8]); 
+			vprint($config, "Could not download $files[8]", "warn") unless (defined($status));
+		}
+		#print "@files[8]\n";
+		#	s/^.*\s(\S+)$/$1/;
+		#print "$_\n";
+#		ftp->get($file, $ldir . $config->{"ospath"} . $file);
+	}
+	chdir ".." && $ftp->cdup();
+	return(0);
+}#}}}
 
 sub FTPinit {#{{{
 	my $config = shift;
 	my $ftp = Net::FTP->new($config->{"server"}, Passive => $config->{"passive"}) or die "Cannot connect to $config->{'server'}";
-	vprint($config, "successfull logged into Server $config->{'server'}");
+	vprint($config, "successfull logged into Server $config->{'server'}", "debug");
 	$ftp->login($config->{"user"}, $config->{"pass"}) or die "Cannot login\n Are Password and Username correct?";
 	 # enable binary mode, if configured
 	if($config{"binary"}){
 		$ftp->binary;
-		vprint($config, "Changed connection to binary mode");
+		vprint($config, "Changed connection to binary mode", "debug");
 	}
 	# Check local Backup Directory
 	eval { 
 		mkdir $config->{"localdir"},0700 unless (-d $config->{"localdir"}); 
-		chmod 0600, $config->{"localdir"} unless (-w $config->{"localdir"});
+		chmod 0700, $config->{"localdir"} unless (-w $config->{"localdir"});
 	};
 	if ($@) {
 		die "Error, setting up Backup Directory $config->{'localdir'}, exiting..."
 	}
 	else { 
-		vprint($config, "$config->{'localdir'} looks alright"); 
+		vprint($config, "$config->{'localdir'} looks alright", "debug"); 
 	}
 	return $ftp;
 }#}}}
 
 sub FTPlist{#{{{
-	(my $config, my $ftp, my $dir)  = @_;
+	(my $ftp, my $dir)  = @_;
 	return $ftp->dir($dir);
 }#}}}
 
@@ -143,20 +118,17 @@ EOF
 }#}}}
 
 sub FTPinitLocal{#{{{
-	my $ftp		 = shift;
 	my $config	 = shift;
-
-	my @list = FTPlist(\%config, $$ftp, ".");
 
 	# Datum im Format YYYYMMDD
 	(my $mday, my $mon, my $year) = (localtime(time))[3,4,5];
 	# Test for existance of directory
 	my $dir = $year+1900 . sprintf("%02d",$mon+1) . $mday;
 	if (-d $config{"localdir"} . $config{"ospath"}.$dir) {
-		vprint($config, "Directory \'$dir\' already exists in $config{'localdir'}");
+		vprint($config, "Directory \'$dir\' already exists in $config{'localdir'}", "debug");
 	}
 	else {
-		mkdir $config{"localdir"} . $config{"ospath"} . $dir, 0600 ||
+		mkdir $config{"localdir"} . $config{"ospath"} . $dir, 0700 ||
 		print "Cannot create Directory $dir in $config{'localdir'}.\n";
 	}
 	return $config{"localdir"} . $config{"ospath"} . $dir;
@@ -170,7 +142,7 @@ sub FTPcheckOldVers{#{{{
 		 # sort reverse by mtime
 		 @temp = sort {(stat($a))[9] <=> (stat($b))[9]} @temp;
 		 while (@temp > $config{'keep'}){
-			 vprint(\%config, "Deleting file $temp[0]");
+			 vprint(\%config, "Deleting file $temp[0]", "debug");
 			 eval{
 				 unlink glob($temp[0].$config{'ospath'}."*");
 				 rmdir shift(@temp);
@@ -181,5 +153,71 @@ sub FTPcheckOldVers{#{{{
 		 }
 		
 	}
-	return 1;
+	return(0);
+}#}}}
+
+sub getConfig(){#{{{
+	
+    # Default Options, these can be overruled
+    # using commandline optins
+	my $user		 = "anonymous";
+	my $password	 = 'none@none.org';
+	my $host		 = "localhost";
+	my $localdir	 = "~/perl_backup";
+	# FTP control port
+	my $port		 = 21;
+	# Enable Passive mode? 1 enables, 0 disables
+	my $passive      = 1;
+	# Enable Recurisve mode? 1 enables, 0 disables
+	my $recursive    = 1;
+	# Enable Debug mode? 1 enables, 0 disables
+	my $debug		 = 1;
+	# Enable binary mode? 1 enables, 0 disables
+	my $binary		 = 1;
+	# Wieviele Versionen behalten:
+	my $keep		 = 3;
+
+	# Welches Directory?
+	my $dir			 = ".";
+
+	# exclude patterns
+	my $exclude;
+
+	# Operating System specific
+	my $ospath  = ($Config{"osname"} =~ "MsWin" ) ? '\\' : '/';
+
+	GetOptions('user=s' => \$user,
+               'pass=s' => \$password,
+               'help'  => sub {usage},
+               'server=s' => \$host,
+               'recursive' => \$recursive,
+               'port=i' => \$port,
+		       'passive=i' => \$passive,
+		       'debug=i'   => \$debug,
+			   'binary=i'  => \$binary,
+			   'dir=s'	   => \$dir,
+			   'localdir=s' => \$localdir);
+
+	my %config = (
+		server	 =>  $host,
+		user	 =>  $user,
+		pass	 =>  $password,
+		passive	 =>	 $passive,
+		debug	 =>	 $debug,
+		binary	 =>	 $binary,
+		localdir =>  $localdir,
+		ospath   =>  $ospath,
+		keep	 =>	 $keep,
+		dir		 =>  $dir
+	);
+	$config{"localdir"}=glob($config{"localdir"});
+
+	return %config;
+}#}}}
+
+sub vprint {#{{{
+	my ($config, $msg, $facility) = @_;
+	if ($config->{"debug"}) {
+		print "[$facility] $msg\n";
+	}
 }#}}}
